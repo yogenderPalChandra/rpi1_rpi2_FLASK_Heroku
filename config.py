ec82123cfaeb38cd6116cf4bdddef93c15307291
yogender <yogender027mae@gmail.com>
2020-12-02 15:52:49 +0000

    dbs switched

--- config.py
+++ config.py
@@ -8,9 +8,12 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = 'bittoo'
     #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SQLALCHEMY_DATABASE_URI = "postgresql://yogi:bittoo@localhost:5432/TemaccessToRemoteRp2"
+    #SQLALCHEMY_DATABASE_URI = "postgresql://yogi:bittoo@localhost:5432/TemaccessToRemoteRp2"
+    QLALCHEMY_DATABASE_URI ="postgresql://yogi:bittoo@10.208.8.121:5432/flow"
     SQLALCHEMY_BINDS = {
-    'flow':     "postgresql://yogi:bittoo@10.208.8.121:5432/flow"}
+    'flow':     "postgresql://yogi:bittoo@localhost:5432/TemaccessToRemoteRp2"}
+    #SQLALCHEMY_BINDS = {
+    #'flow':     "postgresql://yogi:bittoo@10.208.8.121:5432/flow"}
 
 
 class ProductionConfig(Config):
